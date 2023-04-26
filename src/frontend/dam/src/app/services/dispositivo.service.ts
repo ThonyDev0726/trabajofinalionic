4203021ecb37cd73b7eeae2ae318f055c26eacc0
ahidalgov <112734964+ahidalgov@users.noreply.github.com>
2023-04-26 09:14:34 -0500

    Update dispositivo.service.ts

--- src/frontend/dam/src/app/services/dispositivo.service.ts
+++ src/frontend/dam/src/app/services/dispositivo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
+import { Dispositivo } from '../interfaces/dispositivos';
 
 
 @Injectable({
@@ -10,10 +11,9 @@ export class DispositivoService {
 
   constructor(private _http: HttpClient) { }
 
-  getListadoDispositivos (): Promise<any> {
-    return firstValueFrom(this._http.get('http://localhost:8000/devices'))
+  getListadoDispositivos (): Promise<Dispositivo[]> {
+    return firstValueFrom(this._http.get<Dispositivo[]>('http://localhost:8000/devices'))
   }
-
   getListadoElectrovalvulas (): Promise<any> {
     return firstValueFrom(this._http.get('http://localhost:8000/electrovalvulas'))
   }
@@ -21,6 +21,4 @@ export class DispositivoService {
   getListadoMediciones (): Promise<any> {
     return firstValueFrom(this._http.get('http://localhost:8000/mediciones'))
   }
-
 }
-
