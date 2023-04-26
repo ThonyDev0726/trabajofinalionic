6c6fe8410ba6b6d2517e89c4f4a0c732f02a7fc5
ahidalgov <112734964+ahidalgov@users.noreply.github.com>
2023-04-26 09:16:12 -0500

    Update dispositivo.page.ts

--- src/frontend/dam/src/app/dispositivo/dispositivo.page.ts
+++ src/frontend/dam/src/app/dispositivo/dispositivo.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DispositivoService } from '../services/dispositivo.service';
 import { Observable, Subscription, interval } from 'rxjs';
+import { Dispositivo } from '../interfaces/dispositivos';
 
 @Component({
   selector: 'app-dispositivo',
@@ -11,30 +12,33 @@ export class DispositivoPage implements OnInit, OnDestroy {
 
   observable$: Observable<any>
   subscription: Subscription
-  lista: Dispositivo[] = this.dispositivoService.getListadoDispositivos()
-  constructor(private dispositivoService: DispositivoService) {
-   this.observable$ = interval(1000)
+  
+   listadispositivo:Dispositivo[]=this.dispositivoService.getListadoDispositivos();
 
+  constructor(private dispositivoService: DispositivoService) {
+    
+    
+    this.observable$ = interval(1000)
 
     this.subscription = this.observable$.subscribe((integer) => {
       console.log(integer)
     })
   }
-
+  
   async ngOnInit() {
     this.subscription.unsubscribe()
-    this.subscription.leerDispositivo()
     //let dispositivos = await this.dispositivoService.getListadoDispositivos()
     //console.log(dispositivos)
     console.log('Me ejecuto primero')
     this.dispositivoService.getListadoDispositivos()
-    .then((res) => {
-      console.log(res)
-  })
-   .catch((error) => {
-      console.log(error)
-    })
-    this.dispositivoService.getListadoElectrovalvulas()
+       .then((res) => {
+         console.log(res)
+     })
+      .catch((error) => {
+         console.log(error)
+       })
+
+       this.dispositivoService.getListadoElectrovalvulas()
     .then((res) => {
       console.log(res)
   })
@@ -59,16 +63,6 @@ export class DispositivoPage implements OnInit, OnDestroy {
   unsubscribe() {
     this.subscription.unsubscribe()
   }
-
-leerDispositivo(){
- this.subscription.subscribe (
-res => this.lista = res,
-error => console.log (error),
-() => console.log (this.lista)
-
- )
-
-}
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe()
