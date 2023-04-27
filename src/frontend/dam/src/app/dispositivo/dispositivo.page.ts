39988cccd058d5446a3b84338198bc59c3857207
ahidalgov <112734964+ahidalgov@users.noreply.github.com>
2023-04-27 01:29:16 -0500

    Update dispositivo.page.ts

--- src/frontend/dam/src/app/dispositivo/dispositivo.page.ts
+++ src/frontend/dam/src/app/dispositivo/dispositivo.page.ts
@@ -2,6 +2,8 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DispositivoService } from '../services/dispositivo.service';
 import { Observable, Subscription, interval } from 'rxjs';
 import { Dispositivo } from '../interfaces/dispositivos';
+import { Electrovalvulas } from '../interfaces/electrovalvulas';
+import { Mediciones } from '../interfaces/mediciones';
 
 @Component({
   selector: 'app-dispositivo',
@@ -13,10 +15,17 @@ export class DispositivoPage implements OnInit, OnDestroy {
   observable$: Observable<any>
   subscription: Subscription
   
-   listadispositivo:Dispositivo[]=this.dispositivoService.getListadoDispositivos();
+   listadispositivo:Dispositivo[]
+   listaelectrovalvulas:Electrovalvulas[]
+   listamediciones:Mediciones[]
+   
+   
 
   constructor(private dispositivoService: DispositivoService) {
     
+    this.listadispositivo = []
+    this.listaelectrovalvulas =[]
+    this.listamediciones=[]
     
     this.observable$ = interval(1000)
 
@@ -33,6 +42,7 @@ export class DispositivoPage implements OnInit, OnDestroy {
     this.dispositivoService.getListadoDispositivos()
        .then((res) => {
          console.log(res)
+         this.listadispositivo =  res
      })
       .catch((error) => {
          console.log(error)
@@ -41,6 +51,7 @@ export class DispositivoPage implements OnInit, OnDestroy {
        this.dispositivoService.getListadoElectrovalvulas()
     .then((res) => {
       console.log(res)
+      this.listaelectrovalvulas =  res
   })
    .catch((error) => {
       console.log(error)
@@ -48,6 +59,7 @@ export class DispositivoPage implements OnInit, OnDestroy {
     this.dispositivoService.getListadoMediciones()
        .then((res) => {
          console.log(res)
+         this.listamediciones =  res
      })
       .catch((error) => {
          console.log(error)
