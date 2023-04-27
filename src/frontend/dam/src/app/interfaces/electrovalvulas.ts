4ef8deb57ced64895e58c57720d1bf19cf8f699a
ahidalgov <112734964+ahidalgov@users.noreply.github.com>
2023-04-27 01:26:57 -0500

    Add files via upload

---
+++ src/frontend/dam/src/app/interfaces/electrovalvulas.ts
@@ -0,0 +1,5 @@
+export interface Electrovalvulas{
+    electrovalvulaId: number;
+    nombre: string;
+    
+}
---
+++ src/frontend/dam/src/app/interfaces/mediciones.ts
@@ -0,0 +1,6 @@
+export interface Mediciones{
+    fecha: string;
+    valor: string;
+    dispositivoId:number;
+    
+}
